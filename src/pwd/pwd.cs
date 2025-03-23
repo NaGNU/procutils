640264b834596e5a50519ec6c1b004b1f478fad7
ruze42 <malledeferlive@gmail.com>
2025-03-24 00:30:05 +0500

    merge

---
+++ src/pwd/pwd.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+  {
+    public static void Main (
+               string[] args
+              )
+      {  
+          string currentDir = Environment.CurrentDirectory;
+          Console.WriteLine (currentDir);
+      }
+      
+  }
